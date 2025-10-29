fe2e592cc6a027ae98b6e31f180258847ec3772e
Augusto-Viniciuss <augustoviniciud@gmail.com>
2025-10-29 11:18:09 -0300

    first commit

---
+++ ros_packages/laser_fast_lio_core/launch/fast_lio.launch.py
@@ -0,0 +1,59 @@
+import os
+
+from launch import LaunchDescription
+
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    package_path = get_package_share_directory('laser_fast_lio_core')
+    default_config_file_path = os.path.join(package_path, 'params', 'mid360_internal.yaml')
+
+    # Declare arguments
+    declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'namespace',
+            default_value=os.getenv('UAV_NAME', "uav1"),
+            description='Top-level namespace.'))
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value=PythonExpression(['"', os.getenv('REAL_UAV', "true"), '" == "false"']),
+            description='Whether use the simulation time.'))
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'fast_lio_config_file_path',
+            default_value=default_config_file_path,
+            description='Config file path for fast lio.'))
+
+    # Initialize arguments
+    namespace = LaunchConfiguration('namespace')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    fast_lio_config_file_path = LaunchConfiguration('fast_lio_config_file_path')
+
+    # Launch Fast Lio
+    fast_lio_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('fast_lio'),
+                'launch',
+                'mapping.launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+            'config_file_path': fast_lio_config_file_path,
+        }.items()
+    )
+
+    return LaunchDescription(declared_arguments + [fast_lio_launch])
